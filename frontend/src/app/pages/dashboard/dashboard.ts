928ba4a63bff97b2d3544aebb5cb9cf80e46bed0
Rahul Varma <chamarthirahul2105@gmail.com>
2026-04-04 13:56:53 +0530

    data

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Auth } from '../../core/auth/auth';
 import { Router } from '@angular/router';
+import { State } from '../../core/services/state';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
-  constructor(private authService: Auth, private router: Router) {}
+  // Inject BOTH Auth Service for routing AND our new State Service for Cart Data
+  constructor(
+    private authService: Auth, 
+    private router: Router,
+    public stateService: State
+  ) {}
 
   logout() {
     this.authService.logout();
